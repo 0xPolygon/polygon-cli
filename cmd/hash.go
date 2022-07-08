91d00d8b463f704a9dc78aa2cfc180719e8b2fa5
John Hilliard <praetoriansentry@gmail.com>
2022-07-08 18:25:07 -0400

    feat: Adding hash command
    A common use case is to hash some file or value from the command
    line. Stuff like sha1 is readily available, but some methods like
    keccak or ripemd160 are a little harder to come by. I'm adding this to
    create some hash utilities and also just get used to cobra.

---
+++ cmd/hash.go
@@ -0,0 +1,177 @@
+/*
+Copyright © 2022 Polygon <engineering@polygon.technology>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"encoding/hex"
+	"fmt"
+	"hash"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
+
+	"golang.org/x/crypto/blake2b"
+	"golang.org/x/crypto/blake2s"
+	"golang.org/x/crypto/md4"
+	"golang.org/x/crypto/ripemd160"
+	"golang.org/x/crypto/sha3"
+)
+
+var (
+	supportedHashFunctions = []string{
+		"md4",
+		"md5",
+		"sha1",
+		"sha224",
+		"sha256",
+		"sha384",
+		"sha512",
+		"ripemd160",
+		"sha3_224",
+		"sha3_256",
+		"sha3_384",
+		"sha3_512",
+		"sha512_224",
+		"sha512_256",
+		"blake2s_256",
+		"blake2b_256",
+		"blake2b_384",
+		"blake2b_512",
+		"keccak256",
+		"keccak512",
+	}
+	inputFileName *string
+)
+
+// hashCmd represents the hash command
+var hashCmd = &cobra.Command{
+	Use:   fmt.Sprintf("hash [%s]", strings.Join(supportedHashFunctions, "|")),
+	Short: "Simple command line tools for common crypto hashing functions",
+	Long: `Hash Functions
+
+This is a simple command line tool to run common hash functions. If
+the --file argument is provided, we'll attempt to read the file and
+hash it. If additionall data is provided on the command line, that
+will be hashed. Otherwise, we'll attempted to read the standard input.
+`,
+	Run: func(cmd *cobra.Command, args []string) {
+		data, err := getInputData(cmd, args)
+		if err != nil {
+			cmd.PrintErrf("There was a nerror reading input for hashing: %s", err.Error())
+			return
+		}
+		h, err := getHash(args[0])
+		if err != nil {
+			cmd.PrintErrf("There was an error creating the hash function: %s", err.Error())
+			return
+		}
+		h.Write(data)
+		hashOut := h.Sum(nil)
+		cmd.Println(hex.EncodeToString(hashOut))
+
+	},
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 {
+			return fmt.Errorf("expected 1 argument to specify hash function. got %d", len(args))
+		}
+		for _, v := range supportedHashFunctions {
+			if v == args[0] {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("The name %s is not recognized. Please use one of the following: %s", args[0], strings.Join(supportedHashFunctions, ","))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(hashCmd)
+	flagSet := hashCmd.PersistentFlags()
+	inputFileName = flagSet.String("file", "", "Provide a filename to read and hash")
+}
+
+func getHash(name string) (hash.Hash, error) {
+	switch name {
+	case "md4":
+		return md4.New(), nil
+	case "md5":
+		return md5.New(), nil
+	case "sha1":
+		return sha1.New(), nil
+	case "sha224":
+		return sha256.New224(), nil
+	case "sha256":
+		return sha256.New(), nil
+	case "sha384":
+		return sha512.New384(), nil
+	case "sha512":
+		return sha512.New(), nil
+	case "ripemd160":
+		return ripemd160.New(), nil
+	case "sha3_224":
+		return sha3.New224(), nil
+	case "sha3_256":
+		return sha3.New256(), nil
+	case "sha3_384":
+		return sha3.New384(), nil
+	case "sha3_512":
+		return sha3.New512(), nil
+	case "sha512_224":
+		return sha512.New512_224(), nil
+	case "sha512_256":
+		return sha512.New512_256(), nil
+	case "blake2s_256":
+		return blake2s.New256(nil)
+	case "blake2b_256":
+		return blake2b.New256(nil)
+	case "blake2b_384":
+		return blake2b.New384(nil)
+	case "blake2b_512":
+		return blake2b.New512(nil)
+	case "keccak256":
+		return sha3.NewLegacyKeccak256(), nil
+	case "keccak512":
+		return sha3.NewLegacyKeccak512(), nil
+	}
+	var h hash.Hash
+	return h, fmt.Errorf("Unable to create a hash function for %s", name)
+}
+
+func getInputData(cmd *cobra.Command, args []string) ([]byte, error) {
+	// first check and see if we have an input file
+	if inputFileName != nil && *inputFileName != "" {
+		// If we get here, we're going to assume the user
+		// wants to hash a file and we're not going to look
+		// for other input sources
+		return ioutil.ReadFile(*inputFileName)
+	}
+
+	// This is a little tricky. If a user provdes multiple args that aren't quoted, it could be confusing
+	if len(args) > 1 {
+		concat := strings.Join(args[1:], " ")
+		return []byte(concat), nil
+	}
+
+	return ioutil.ReadAll(os.Stdin)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,12 @@ module github.com/maticnetwork/polygon-cli
 go 1.18
 
 require (
+	github.com/spf13/cobra v1.5.0
+	github.com/spf13/viper v1.12.0
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
+)
+
+require (
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
@@ -12,10 +18,8 @@ require (
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
-	github.com/spf13/cobra v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.12.0 // indirect
 	github.com/subosito/gotenv v1.3.0 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/text v0.3.7 // indirect
