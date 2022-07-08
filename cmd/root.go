5989f8131478cefe37dcb3505ffe5a88da3d2a18
John Hilliard <praetoriansentry@gmail.com>
2022-07-08 11:13:46 -0400

    feat: Swapping names and cobra config

--- cmd/root.go
+++ cmd/root.go
@@ -1,5 +1,5 @@
 /*
-Copyright © 2022 John Hilliard jhilliard@polygon.technology
+Copyright © 2022 Polygon <engineering@polygon.technology>
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
