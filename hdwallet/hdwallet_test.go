4c5d1d9719908588f33406c3d417c6fafe1ab425
John Hilliard <praetoriansentry@gmail.com>
2022-07-22 13:29:34 -0400

    test: bip32 vec 2

--- hdwallet/hdwallet_test.go
+++ hdwallet/hdwallet_test.go
@@ -60,3 +60,36 @@ func TestBIP32Vec1(t *testing.T) {
 
 	}
 }
+func TestBIP32Vec2(t *testing.T) {
+	seed := "fffcf9f6f3f0edeae7e4e1dedbd8d5d2cfccc9c6c3c0bdbab7b4b1aeaba8a5a29f9c999693908d8a8784817e7b7875726f6c696663605d5a5754514e4b484542"
+	rawSeed, err := hex.DecodeString(seed)
+	if err != nil {
+		t.Fatalf("Failed to parse seed hex: %v", err)
+	}
+	pw, _ := NewPolyWalletFromSeed(rawSeed)
+	pw.rawSeed = rawSeed
+
+	table := map[string][2]string{
+		"m":                               {"xpub661MyMwAqRbcFW31YEwpkMuc5THy2PSt5bDMsktWQcFF8syAmRUapSCGu8ED9W6oDMSgv6Zz8idoc4a6mr8BDzTJY47LJhkJ8UB7WEGuduB", "xprv9s21ZrQH143K31xYSDQpPDxsXRTUcvj2iNHm5NUtrGiGG5e2DtALGdso3pGz6ssrdK4PFmM8NSpSBHNqPqm55Qn3LqFtT2emdEXVYsCzC2U"},
+		"m/0":                             {"xpub69H7F5d8KSRgmmdJg2KhpAK8SR3DjMwAdkxj3ZuxV27CprR9LgpeyGmXUbC6wb7ERfvrnKZjXoUmmDznezpbZb7ap6r1D3tgFxHmwMkQTPH", "xprv9vHkqa6EV4sPZHYqZznhT2NPtPCjKuDKGY38FBWLvgaDx45zo9WQRUT3dKYnjwih2yJD9mkrocEZXo1ex8G81dwSM1fwqWpWkeS3v86pgKt"},
+		"m/0/2147483647'":                 {"xpub6ASAVgeehLbnwdqV6UKMHVzgqAG8Gr6riv3Fxxpj8ksbH9ebxaEyBLZ85ySDhKiLDBrQSARLq1uNRts8RuJiHjaDMBU4Zn9h8LZNnBC5y4a", "xprv9wSp6B7kry3Vj9m1zSnLvN3xH8RdsPP1Mh7fAaR7aRLcQMKTR2vidYEeEg2mUCTAwCd6vnxVrcjfy2kRgVsFawNzmjuHc2YmYRmagcEPdU9"},
+		"m/0/2147483647'/1":               {"xpub6DF8uhdarytz3FWdA8TvFSvvAh8dP3283MY7p2V4SeE2wyWmG5mg5EwVvmdMVCQcoNJxGoWaU9DCWh89LojfZ537wTfunKau47EL2dhHKon", "xprv9zFnWC6h2cLgpmSA46vutJzBcfJ8yaJGg8cX1e5StJh45BBciYTRXSd25UEPVuesF9yog62tGAQtHjXajPPdbRCHuWS6T8XA2ECKADdw4Ef"},
+		"m/0/2147483647'/1/2147483646'":   {"xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL", "xprvA1RpRA33e1JQ7ifknakTFpgNXPmW2YvmhqLQYMmrj4xJXXWYpDPS3xz7iAxn8L39njGVyuoseXzU6rcxFLJ8HFsTjSyQbLYnMpCqE2VbFWc"},
+		"m/0/2147483647'/1/2147483646'/2": {"xpub6FnCn6nSzZAw5Tw7cgR9bi15UV96gLZhjDstkXXxvCLsUXBGXPdSnLFbdpq8p9HmGsApME5hQTZ3emM2rnY5agb9rXpVGyy3bdW6EEgAtqt", "xprvA2nrNbFZABcdryreWet9Ea4LvTJcGsqrMzxHx98MMrotbir7yrKCEXw7nadnHM8Dq38EGfSh6dqA9QWTyefMLEcBYJUuekgW4BYPJcr9E7j"},
+	}
+	for k, v := range table {
+		key, err := pw.GetKeyForPath(k)
+		if err != nil {
+			t.Fatalf("Unable to generate key for path %s %v", k, err)
+		}
+		prvData := key.String()
+		pubData := key.PublicKey().String()
+		if prvData != v[1] {
+			t.Fatalf("Private key for path %s was mismatched. Expected %s got %s", k, v[1], prvData)
+		}
+		if pubData != v[0] {
+			t.Fatalf("Public key for path %s was mismatched. Expected %s got %s", k, v[0], pubData)
+		}
+
+	}
+}
