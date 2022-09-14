e22cc48a583231477e248ee7b416955777b4b6a5
John Hilliard <praetoriansentry@gmail.com>
2022-09-13 22:53:45 -0400

    ops: adding smart contracts
    Adding some left over smart contracts

---
+++ cmd/forge.go
@@ -0,0 +1,267 @@
+/*
+Copyright © 2022 Polygon <engineering@polygon.technology>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"os"
+	"time"
+
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/consensus/ethash"
+	ethcore "github.com/ethereum/go-ethereum/core"
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	ethparams "github.com/ethereum/go-ethereum/params"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+)
+
+var (
+	inputForgeFile      *string
+	inputForgeCache     *int
+	inputForgeHandles   *int
+	inputForgeAncient   *string
+	inputForgeNamespace *string
+	inputForgeEngine    *string
+	inputForgeChainID   *int
+)
+
+type (
+	fakeBlockReader struct {
+		isInitialized bool
+		scanner       *bufio.Scanner
+	}
+)
+
+func (f *fakeBlockReader) Init() {
+	f.scanner = bufio.NewScanner(os.Stdin)
+	f.isInitialized = true
+}
+
+func (f *fakeBlockReader) GetFakeBlock() (*ethtypes.Header, []*ethtypes.Transaction, error) {
+	if !f.scanner.Scan() {
+		return nil, nil, fmt.Errorf("No more fake blocks left")
+	}
+
+	blockText := f.scanner.Bytes()
+	var raw json.RawMessage
+	err := json.Unmarshal(blockText, &raw)
+	if err != nil {
+		return nil, nil, err
+	}
+	// Decode header and transactions.
+	var head *ethtypes.Header
+	var body rpcBlock
+	if err := json.Unmarshal(raw, &head); err != nil {
+		return nil, nil, err
+	}
+	if err := json.Unmarshal(raw, &body); err != nil {
+		return nil, nil, err
+	}
+
+	txs := make([]*ethtypes.Transaction, len(body.Transactions))
+	for i, tx := range body.Transactions {
+		txs[i] = tx.tx
+	}
+	return head, txs, nil
+
+}
+
+// forgeCmd represents the forge command
+var forgeCmd = &cobra.Command{
+	Use:   "forge",
+	Short: "Forge a fake state",
+	Long: `
+This command expects input to be a stream of json encoded block
+objects. The goal is to take those blocks and write them into a
+database in order to forge a long and complicated history for new
+block chain clients and tests.
+
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		db, err := rawdb.NewLevelDBDatabaseWithFreezer(*inputForgeFile, *inputForgeCache, *inputForgeHandles, *inputForgeAncient, *inputForgeNamespace, false)
+		if err != nil {
+			return err
+		}
+
+		fbr := new(fakeBlockReader)
+		fbr.Init()
+		// Idea here is that different engines could be used
+		// to forge the blocks. We'll read the input and
+		// decide which engin to instantiate
+		switch *inputForgeEngine {
+		case "ethhash":
+			config := ethparams.ChainConfig{ChainID: big.NewInt(int64(*inputForgeChainID))}
+			gen := &ethcore.Genesis{
+				Config:     &config,
+				Nonce:      0,
+				Timestamp:  uint64(time.Unix(0, 0).Unix()),
+				ParentHash: ethcommon.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
+				ExtraData:  nil,
+				GasLimit:   20_000_000,
+				GasUsed:    0,
+				Difficulty: big.NewInt(0),
+				Mixhash:    ethcommon.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000"),
+				Coinbase:   ethcommon.HexToAddress("0x0000000000000000000000000000000000000000000000000000000000000000"),
+				Alloc:      make(map[ethcommon.Address]ethcore.GenesisAccount, 0),
+				BaseFee:    big.NewInt(0),
+			}
+
+			jsonData, err := gen.MarshalJSON()
+			if err != nil {
+				return err
+			}
+
+			err = os.WriteFile(*inputForgeFile+"/genesis.json", jsonData, 0777)
+			if err != nil {
+				return err
+			}
+
+			gblock, err := gen.Commit(db)
+			if err != nil {
+				return err
+			}
+
+			engine := ethash.NewFaker()
+
+			blocks, receipts := ethcore.GenerateChain(&config, gblock, engine, db, 100000, func(i int, gen *ethcore.BlockGen) {
+				genBlock(i, gen, fbr)
+			})
+			_ = blocks
+			_ = receipts
+
+		}
+
+		return err
+	},
+}
+
+func genBlock(blockNum int, bg *ethcore.BlockGen, fbr *fakeBlockReader) {
+	log.Trace().Int("blocknumber", blockNum).Msg("Forging")
+	_, txs, err := fbr.GetFakeBlock()
+
+	if err != nil {
+		log.Error().Err(err).Msg("Unable to get a fake block")
+		return
+	}
+
+	for _, v := range txs {
+		if v == nil {
+			continue
+		}
+		bg.AddTx(v)
+	}
+}
+
+func readBlocks(bc *ethcore.BlockChain, gblock *ethtypes.Block) error {
+	scanner := bufio.NewScanner(os.Stdin)
+	parentHash := gblock.Hash()
+	parentNumber := gblock.Number()
+	for scanner.Scan() {
+		blockText := scanner.Bytes()
+		var raw json.RawMessage
+		err := json.Unmarshal(blockText, &raw)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		b, err := readBlock(raw, parentHash, parentNumber)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
+		parentHash = b.Hash()
+		parentNumber = b.Number()
+		fmt.Printf("Forging block: %d\n", b.Number())
+		fmt.Println(b)
+
+		_, err = bc.InsertChain(ethtypes.Blocks{b})
+		if err != nil {
+			fmt.Println(err.Error())
+			return err
+		}
+	}
+	return nil
+}
+
+type rpcBlock struct {
+	Hash         ethcommon.Hash   `json:"hash"`
+	Transactions []rpcTransaction `json:"transactions"`
+	UncleHashes  []ethcommon.Hash `json:"uncles"`
+}
+type rpcTransaction struct {
+	tx *ethtypes.Transaction
+	txExtraInfo
+}
+type txExtraInfo struct {
+	BlockNumber *string            `json:"blockNumber,omitempty"`
+	BlockHash   *ethcommon.Hash    `json:"blockHash,omitempty"`
+	From        *ethcommon.Address `json:"from,omitempty"`
+}
+
+// https://github.com/ethereum/go-ethereum/blob/d901d85377c2c2f05f09f423c7d739c0feecd90a/ethclient/ethclient.go#L110
+func readBlock(raw json.RawMessage, parentHash ethcommon.Hash, parentNumber *big.Int) (*ethtypes.Block, error) {
+	// Decode header and transactions.
+	var head *ethtypes.Header
+	var body rpcBlock
+	if err := json.Unmarshal(raw, &head); err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal(raw, &body); err != nil {
+		return nil, err
+	}
+	// Quick-verify transaction and uncle lists. This mostly helps with debugging the server.
+	if head.UncleHash == ethtypes.EmptyUncleHash && len(body.UncleHashes) > 0 {
+		return nil, fmt.Errorf("server returned non-empty uncle list but block header indicates no uncles")
+	}
+	if head.UncleHash != ethtypes.EmptyUncleHash && len(body.UncleHashes) == 0 {
+		return nil, fmt.Errorf("server returned empty uncle list but block header indicates uncles")
+	}
+	if head.TxHash == ethtypes.EmptyRootHash && len(body.Transactions) > 0 {
+		return nil, fmt.Errorf("server returned non-empty transaction list but block header indicates no transactions")
+	}
+	if head.TxHash != ethtypes.EmptyRootHash && len(body.Transactions) == 0 {
+		return nil, fmt.Errorf("server returned empty transaction list but block header indicates transactions")
+	}
+	txs := make([]*ethtypes.Transaction, len(body.Transactions))
+	for i, tx := range body.Transactions {
+		txs[i] = tx.tx
+	}
+	head.ParentHash = parentHash
+	parentNumber.Add(parentNumber, big.NewInt(1))
+	head.Number = parentNumber
+	head.Difficulty = big.NewInt(131072)
+	head.GasLimit = 20000000
+	head.MixDigest = ethcommon.HexToHash("0x0000000000000000000000000000000000000000000000000000000000000000")
+
+	head.Time = uint64(time.Now().Unix())
+	return ethtypes.NewBlockWithHeader(head).WithBody(txs, nil), nil
+}
+
+func init() {
+	rootCmd.AddCommand(forgeCmd)
+	inputForgeFile = forgeCmd.PersistentFlags().StringP("datadir", "f", "./chaindata", "")
+	inputForgeCache = forgeCmd.PersistentFlags().IntP("cache", "c", 512, "")
+	inputForgeHandles = forgeCmd.PersistentFlags().Int("handles", 5120, "")
+	inputForgeAncient = forgeCmd.PersistentFlags().StringP("datadir.ancient", "a", "./chaindata/ancient", "")
+	inputForgeNamespace = forgeCmd.PersistentFlags().StringP("namespace", "n", "eth/db/chaindata/", "")
+	inputForgeChainID = forgeCmd.PersistentFlags().IntP("chain-id", "i", 1337, "The chain id to use when configuring the blockchain")
+	inputForgeEngine = forgeCmd.PersistentFlags().StringP("engine", "e", "ethhash", "The engine to use for the blockchain")
+}
--- go.mod
+++ go.mod
@@ -19,26 +19,44 @@ require (
 	github.com/StackExchange/wmi v0.0.0-20180116203802-5d049714c4a6 // indirect
 	github.com/VictoriaMetrics/fastcache v1.6.0 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
-	github.com/cespare/xxhash/v2 v2.1.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/deckarep/golang-set v1.8.0 // indirect
-	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/edsrzf/mmap-go v1.0.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.4 // indirect
 	github.com/gizak/termui/v3 v3.1.0 // indirect
 	github.com/go-ole/go-ole v1.2.1 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
-	github.com/google/uuid v1.2.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
 	github.com/hashicorp/golang-lru v0.5.5-0.20210104140557-80c98217689d // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/holiman/bloomfilter/v2 v2.0.3 // indirect
+	github.com/holiman/uint256 v1.2.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/ipfs/go-cid v0.2.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.1.0 // indirect
+	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
+	github.com/libp2p/go-libp2p v0.22.0 // indirect
+	github.com/libp2p/go-openssl v0.1.0 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-pointer v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mitchellh/go-wordwrap v0.0.0-20150314170334-ad45545899c7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-base32 v0.0.4 // indirect
+	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multiaddr v0.6.0 // indirect
+	github.com/multiformats/go-multibase v0.1.1 // indirect
+	github.com/multiformats/go-multicodec v0.5.0 // indirect
+	github.com/multiformats/go-multihash v0.2.1 // indirect
+	github.com/multiformats/go-varint v0.0.6 // indirect
 	github.com/nsf/termbox-go v0.0.0-20190121233118-02980233997d // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
@@ -48,6 +66,8 @@ require (
 	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/rs/zerolog v1.27.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
+	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
 	github.com/spf13/afero v1.8.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -56,11 +76,12 @@ require (
 	github.com/syndtr/goleveldb v1.0.1-0.20220614013038-64ee5596c38a // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
-	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20220722155302-e5dcc9cfc0b9 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	lukechampine.com/blake3 v1.1.7 // indirect
 )
