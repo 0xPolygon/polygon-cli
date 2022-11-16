6acf522b20ca23b2ec9d46b78eebbaee8940ed78
leovct <l009.vincent@gmail.com>
2022-11-16 12:15:03 +0100

    test: add test for other nodekey functions

--- cmd/nodekey_test.go
+++ cmd/nodekey_test.go
@@ -6,17 +6,77 @@ import (
 	libp2pcrypto "github.com/libp2p/go-libp2p/core/crypto"
 )
 
+func TestGenerateETHNodeKey(t *testing.T) {
+	res, err := generateETHNodeKey()
+	if err != nil {
+		t.Errorf("could not generate eth key: %v", err)
+	} else {
+		if len(res.PublicKey) == 0 {
+			t.Errorf("eth public key is empty")
+		}
+		if len(res.PrivateKey) == 0 {
+			t.Errorf("eth private key is empty")
+		}
+		if len(res.ENR) == 0 {
+			t.Errorf("enr is empty")
+		}
+	}
+}
+
+func TestGenerateLibp2pNodeKey(t *testing.T) {
+	type test struct {
+		name    string
+		keyType int
+	}
+
+	tests := []test{
+		{
+			name:    "ed25519",
+			keyType: libp2pcrypto.Ed25519,
+		},
+		{
+			name:    "secp256k1",
+			keyType: libp2pcrypto.Secp256k1,
+		},
+		{
+			name:    "ecdsa",
+			keyType: libp2pcrypto.ECDSA,
+		},
+		{
+			name:    "rsa",
+			keyType: libp2pcrypto.RSA,
+		},
+	}
+
+	for _, test := range tests {
+		res, err := generateLibp2pNodeKey(test.keyType)
+		if err != nil {
+			t.Errorf("could not generate %v key: %v", test.name, err)
+		} else {
+			if len(res.PublicKey) == 0 {
+				t.Errorf("%v public key is empty", test.name)
+			}
+			if len(res.PrivateKey) == 0 {
+				t.Errorf("%v private key is empty", test.name)
+			}
+			if len(res.FullPrivateKey) == 0 {
+				t.Errorf("%v full private key is empty", test.name)
+			}
+		}
+	}
+}
+
 func TestGenerateSeededLibp2pNodeKey(t *testing.T) {
 	type test struct {
 		name     string
-		param    int
+		keyType  int
 		expected nodeKeyOut
 	}
 
 	tests := []test{
 		{
-			name:  "ed25519 key with default seed",
-			param: libp2pcrypto.Ed25519,
+			name:    "ed25519 key with default seed",
+			keyType: libp2pcrypto.Ed25519,
 			expected: nodeKeyOut{
 				PublicKey:      "12D3KooWMQMaVofHvQgjffhQGy3RmBRERFJxSJy59BgwTFnYASqX",
 				PrivateKey:     "00000000000425d4000000000000000000000000000000000000000000000000",
@@ -24,8 +84,8 @@ func TestGenerateSeededLibp2pNodeKey(t *testing.T) {
 			},
 		},
 		{
-			name:  "secp256k1 key with default seed",
-			param: libp2pcrypto.Secp256k1,
+			name:    "secp256k1 key with default seed",
+			keyType: libp2pcrypto.Secp256k1,
 			expected: nodeKeyOut{
 				PublicKey:      "16Uiu2HAm7WUXemKojg36e3P5pBwUAZoJCxBUmTXqeMMUaDxqkU6H",
 				PrivateKey:     "08021220f59d925597656394ac83db5c280338c8c253982ffeea3f71f4dbe329",
@@ -33,8 +93,8 @@ func TestGenerateSeededLibp2pNodeKey(t *testing.T) {
 			},
 		},
 		{
-			name:  "ecdsa key with default seed",
-			param: libp2pcrypto.ECDSA,
+			name:    "ecdsa key with default seed",
+			keyType: libp2pcrypto.ECDSA,
 			expected: nodeKeyOut{
 				PublicKey:      "Qme51zsx66xx4zAo7KZoRKf45hQNEX3jC9Wb3ghUi5GduZ",
 				PrivateKey:     "30770201010420000425d3fffbda2c000000000001164e43c6b2acbc73b3c524",
@@ -43,13 +103,13 @@ func TestGenerateSeededLibp2pNodeKey(t *testing.T) {
 		},
 		{
 			name:     "rsa key with default seed",
-			param:    libp2pcrypto.RSA,
+			keyType:  libp2pcrypto.RSA,
 			expected: nodeKeyOut{},
 		},
 	}
 
 	for _, test := range tests {
-		res, err := generateSeededLibp2pNodeKey(test.param)
+		res, err := generateSeededLibp2pNodeKey(test.keyType)
 		if err != nil {
 			t.Errorf("test %v: %v", test.name, err)
 		} else {
