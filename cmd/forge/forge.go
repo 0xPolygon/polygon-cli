2ef0c67c258757f31585fb47883f6e8d54e8922d
minhd-vu <minhd_vu@yahoo.com>
2023-01-20 11:01:33 -0500

    proto optimizations

--- cmd/forge/forge.go
+++ cmd/forge/forge.go
@@ -44,7 +44,6 @@ import (
 	"github.com/maticnetwork/polygon-cli/proto/gen/pb"
 	"github.com/maticnetwork/polygon-cli/rpctypes"
 	"golang.org/x/exp/slices"
-	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 
 	"github.com/rs/zerolog"
@@ -300,14 +299,59 @@ func (br *ProtoBlockReader) ReadBlock() (rpctypes.PolyBlock, error) {
 	}
 
 	br.offset += int64(itemSize)
-	rawBlockBytes, err := protojson.Marshal(block)
-	if err != nil {
-		return nil, err
+
+	txs := []rpctypes.RawTransactionResponse{}
+	for _, tx := range block.Transactions {
+		to := ""
+		if tx.To != nil {
+			to = *tx.To
+		}
+
+		txs = append(txs, rpctypes.RawTransactionResponse{
+			BlockHash:        rpctypes.RawData32Response(tx.BlockHash),
+			BlockNumber:      rpctypes.RawQuantityResponse(tx.BlockNumber),
+			From:             rpctypes.RawData20Response(tx.From),
+			Gas:              rpctypes.RawQuantityResponse(tx.Gas),
+			GasPrice:         rpctypes.RawQuantityResponse(tx.GasPrice),
+			Hash:             rpctypes.RawData32Response(tx.Hash),
+			Input:            rpctypes.RawDataResponse(tx.Input),
+			Nonce:            rpctypes.RawQuantityResponse(tx.Nonce),
+			To:               rpctypes.RawData20Response(to),
+			TransactionIndex: rpctypes.RawQuantityResponse(tx.TransactionIndex),
+			Value:            rpctypes.RawQuantityResponse(tx.Value),
+			V:                rpctypes.RawQuantityResponse(tx.V),
+			R:                rpctypes.RawQuantityResponse(tx.R),
+			S:                rpctypes.RawQuantityResponse(tx.S),
+			Type:             rpctypes.RawQuantityResponse(tx.Type),
+		})
 	}
 
-	var raw rpctypes.RawBlockResponse
-	if err = json.Unmarshal(rawBlockBytes, &raw); err != nil {
-		return nil, err
+	uncles := []rpctypes.RawData32Response{}
+	for _, uncle := range block.Uncles {
+		uncles = append(uncles, rpctypes.RawData32Response(uncle))
+	}
+
+	raw := rpctypes.RawBlockResponse{
+		Number:           rpctypes.RawQuantityResponse(block.Number),
+		Hash:             rpctypes.RawData32Response(block.Hash),
+		ParentHash:       rpctypes.RawData32Response(block.ParentHash),
+		Nonce:            rpctypes.RawData8Response(block.Nonce),
+		SHA3Uncles:       rpctypes.RawData32Response(block.Sha3Uncles),
+		LogsBloom:        rpctypes.RawData256Response(block.LogsBloom),
+		TransactionsRoot: rpctypes.RawData32Response(block.TransactionsRoot),
+		StateRoot:        rpctypes.RawData32Response(block.StateRoot),
+		ReceiptsRoot:     rpctypes.RawData32Response(block.ReceiptsRoot),
+		Miner:            rpctypes.RawData20Response(block.Miner),
+		Difficulty:       rpctypes.RawQuantityResponse(block.Difficulty),
+		TotalDifficulty:  rpctypes.RawQuantityResponse(block.TotalDifficulty),
+		ExtraData:        rpctypes.RawDataResponse(block.ExtraData),
+		Size:             rpctypes.RawQuantityResponse(block.Size),
+		GasLimit:         rpctypes.RawQuantityResponse(block.GasLimit),
+		GasUsed:          rpctypes.RawQuantityResponse(block.GasUsed),
+		Timestamp:        rpctypes.RawQuantityResponse(block.Timestamp),
+		Transactions:     txs,
+		Uncles:           uncles,
+		BaseFeePerGas:    rpctypes.RawQuantityResponse(block.BaseFeePerGas),
 	}
 
 	return rpctypes.NewPolyBlock(&raw), nil
