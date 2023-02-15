2db6630148ad2cbc6d3a69262a02fbad1f3ae2c5
John Hilliard <praetoriansentry@gmail.com>
2023-02-15 17:18:32 -0500

    feat: adding input data parsing

--- cmd/abi/abi.go
+++ cmd/abi/abi.go
@@ -3,6 +3,7 @@ package abi
 import (
 	"bytes"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	gethabi "github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/spf13/cobra"
@@ -13,6 +14,7 @@ import (
 
 var (
 	inputFileName *string
+	inputData     *string
 )
 
 var ABICmd = &cobra.Command{
@@ -42,6 +44,27 @@ go run main.go abi < ../zkevm-node/etherman/smartcontracts/abi/polygonzkevm.abi
 		for _, meth := range abi.Methods {
 			fmt.Printf("Selector:%s\tSignature:%s\n", hex.EncodeToString(meth.ID), meth)
 		}
+		if *inputData != "" {
+			id, callData, err := parseContractInputData(*inputData)
+			if err != nil {
+				return err
+			}
+			meth, err := abi.MethodById(id)
+			if err != nil {
+				return err
+			}
+			if meth == nil {
+				return fmt.Errorf("the function selector %s wasn't matched in the given abi", hex.EncodeToString(id))
+			}
+			inputVals := make(map[string]any, 0)
+			err = meth.Inputs.UnpackIntoMap(inputVals, callData)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Input data:")
+			prettyInput, _ := json.MarshalIndent(inputVals, "", "  ")
+			fmt.Println(string(prettyInput))
+		}
 		return nil
 	},
 	Args: func(cmd *cobra.Command, args []string) error {
@@ -52,6 +75,28 @@ go run main.go abi < ../zkevm-node/etherman/smartcontracts/abi/polygonzkevm.abi
 func init() {
 	flagSet := ABICmd.PersistentFlags()
 	inputFileName = flagSet.String("file", "", "Provide a filename to read and analyze")
+	inputData = flagSet.String("data", "", "Provid input data to be unpacked based on the ABI defintion")
+}
+
+func parseContractInputData(data string) ([]byte, []byte, error) {
+	// "0x11223344"
+	if len(data) < 10 {
+		return nil, nil, fmt.Errorf("the input %s is too short for a function call. It should start with 0x and needs at least 4 bytes for a function selector", data)
+	}
+	if data[0:2] != "0x" {
+		return nil, nil, fmt.Errorf("the input data must start with 0x")
+	}
+	// drop the 0x and select the next bytes to represent the selector
+	stringId := data[2:10]
+	rawId, err := hex.DecodeString(stringId)
+	if err != nil {
+		return nil, nil, err
+	}
+	rawCallData, err := hex.DecodeString(data[10:])
+	if err != nil {
+		return nil, nil, err
+	}
+	return rawId, rawCallData, err
 }
 
 func getInputData(cmd *cobra.Command, args []string) ([]byte, error) {
