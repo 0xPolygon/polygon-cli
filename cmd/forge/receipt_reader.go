84b42a4ea701684f73b4d1a759b1796b40132ca0
Minh Vu <minhd_vu@yahoo.com>
2023-02-23 16:28:19 -0500

    [DVT-380] Add block rewards (#42)
    * add basic block reward flag
    
    * refactor block reader, add receipt reader, forge reads receipts
    
    * update receipt, add block reward calculation
    
    * fix lint
    
    * make max capacity a const
    
    * fix typo

---
+++ cmd/forge/receipt_reader.go
@@ -0,0 +1,58 @@
+package forge
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/maticnetwork/polygon-cli/rpctypes"
+)
+
+type (
+	ReceiptReader interface {
+		ReadReceipt() (*rpctypes.RawTxReceipt, error)
+	}
+	JSONReceiptReader struct {
+		scanner *bufio.Scanner
+	}
+)
+
+// OpenReceiptReader returns a receipt reader object which can be used to read the
+// file. It will return a mode specific receipt reader.
+func OpenReceiptReader(file string, mode string) (ReceiptReader, error) {
+	receiptsFile, err := os.Open(file)
+	if err != nil {
+		return nil, fmt.Errorf("unable to open %s receipts file: %w", file, err)
+	}
+
+	switch mode {
+	case "json":
+		buf := make([]byte, maxBufferCapacity)
+		scanner := bufio.NewScanner(receiptsFile)
+		scanner.Buffer(buf, maxBufferCapacity)
+
+		receiptsReader := JSONReceiptReader{
+			scanner: scanner,
+		}
+		return &receiptsReader, nil
+
+	default:
+		return nil, fmt.Errorf("invalid mode: %s", mode)
+	}
+}
+
+func (receiptsReader *JSONReceiptReader) ReadReceipt() (*rpctypes.RawTxReceipt, error) {
+	if !receiptsReader.scanner.Scan() {
+		return nil, BlockReadEOF
+	}
+
+	rawTxBytes := receiptsReader.scanner.Bytes()
+	var raw rpctypes.RawTxReceipt
+	err := json.Unmarshal(rawTxBytes, &raw)
+	if err != nil {
+		return nil, fmt.Errorf("unable to unmarshal file receipt: %w - %s", err, string(rawTxBytes))
+	}
+
+	return &raw, nil
+}
