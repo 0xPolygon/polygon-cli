88cb98390879002c1d7931e7aca357c72a68acf7
Jesse Lee <gatsby1118@gmail.com>
2023-06-30 10:09:12 -0400

    put back

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -270,6 +270,7 @@ func GetSimpleTxFields(tx rpctypes.PolyTransaction, chainID, baseFee *big.Int) [
 	fields = append(fields, fmt.Sprintf("Nonce: %d", tx.Nonce()))
 	fields = append(fields, fmt.Sprintf("Type: %d", tx.Type()))
 	fields = append(fields, fmt.Sprintf("Data Len: %d", len(tx.Data())))
+	fields = append(fields, fmt.Sprintf("Data: %s", hex.EncodeToString(tx.Data())))
 
 	return fields
 }
