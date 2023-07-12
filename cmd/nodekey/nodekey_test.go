541dfb62e88731813825434864baa1e68e6a8353
Jesse Lee <gatsby1118@gmail.com>
2023-07-12 11:39:06 -0400

    node-key test

--- cmd/nodekey/nodekey_test.go
+++ cmd/nodekey/nodekey_test.go
@@ -81,7 +81,7 @@ func TestGenerateSeededLibp2pNodeKey(t *testing.T) {
 				PublicKey:      "12D3KooWMQMaVofHvQgjffhQGy3RmBRERFJxSJy59BgwTFnYASqX",
 				PrivateKey:     "00000000000425d4000000000000000000000000000000000000000000000000",
 				FullPrivateKey: "00000000000425d4000000000000000000000000000000000000000000000000ac25a2d49f6266b0a513cf0caf9ea45a9d74d74a1131d5530ac3291d70e81d7a",
-			}, // QmTN9mkNhtUfQ37ZJ1RaepMNBnTQUFFm38fCZgvv6gWzCe
+			},
 		},
 		{
 			name:    "ecdsa key with default seed",
@@ -100,13 +100,13 @@ func TestGenerateSeededLibp2pNodeKey(t *testing.T) {
 			t.Errorf("test %v: %v", test.name, err)
 		} else {
 			if res.PublicKey != test.expected.PublicKey {
-				t.Errorf("test %v: public keys do not match", test.name)
+				t.Errorf("test %s: public keys do not match", res.PublicKey)
 			}
 			if res.PrivateKey != test.expected.PrivateKey {
-				t.Errorf("test %v: private keys do not match", test.name)
+				t.Errorf("test %s: private keys do not match", res.PrivateKey)
 			}
 			if res.FullPrivateKey != test.expected.FullPrivateKey {
-				t.Errorf("test %v: full private keys do not match", test.name)
+				t.Errorf("test %s: full private keys do not match", res.FullPrivateKey)
 			}
 		}
 	}
