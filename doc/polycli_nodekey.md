0dadc2b96252922a2a354869eff56392a14cd10f
John Hilliard <praetoriansentry@gmail.com>
2023-09-24 18:31:58 -0400

    Documentation Changes (#126)
    * docs: some docs cleanup after removing some avail commands
    
    * doc: minor comment for future generation
    
    * docs: updating the mode documentation
    
    ---------
    
    Co-authored-by: Léo Vincent <28714795+leovct@users.noreply.github.com>

--- doc/polycli_nodekey.md
+++ doc/polycli_nodekey.md
@@ -19,15 +19,18 @@ polycli nodekey [flags]
 
 ## Usage
 
-The `nodekey` command is still in progress, but the idea is to have a simple command for generating a node key.
+The `nodekey` command is still in progress, but the idea is to have a
+simple command for generating a node key.
 
-Most clients will generate this on the fly, but if we want to store the key pair during an automated provisioning process, it's helpful to have the output be structured.
+Most clients will generate this on the fly, but if we want to store
+the key pair during an automated provisioning process, it's helpful to
+have the output be structured.
 
 ```bash
 # This will generate a secp256k1 key for devp2p protocol.
 $ polycli nodekey
 
-# Generate a networking keypair for avail.
+# Generate a networking keypair for libp2p.
 $ polycli nodekey --protocol libp2p
 
 # Generate a networking keypair for edge.
--- scripts/clients.mk
+++ scripts/clients.mk
@@ -5,10 +5,6 @@ PORT?=8545
 geth: ## Start a local geth node.
 	geth --dev --dev.period 2 --http --http.addr localhost --http.port $(PORT) --http.api admin,debug,web3,eth,txpool,personal,miner,net --verbosity 5 --rpc.gascap 50000000  --rpc.txfeecap 0 --miner.gaslimit  10 --miner.gasprice 1 --gpo.blocks 1 --gpo.percentile 1 --gpo.maxprice 10 --gpo.ignoreprice 2 --dev.gaslimit 50000000
 
-.PHONY: avail
-avail: ## Start a local avail node.
-	avail --dev --rpc-port $(PORT)
-
 LOADTEST_ACCOUNT=0x85da99c8a7c2c95964c8efd687e95e632fc533d6
 LOADTEST_FUNDING_AMOUNT_ETH=5000
 eth_coinbase := $(shell curl -s -H 'Content-Type: application/json' -d '{"jsonrpc": "2.0", "id": 2, "method": "eth_coinbase", "params": []}' http://127.0.0.1:${PORT} | jq -r ".result")
@@ -19,6 +15,3 @@ geth-loadtest: build ## Fund test account with 5k ETH and run loadtest against a
 	sleep 5
 	$(BUILD_DIR)/$(BIN_NAME) loadtest --verbosity 700 --chain-id 1337 --concurrency 1 --requests 1000 --rate-limit 100 --mode r --legacy http://127.0.0.1:$(PORT)
 
-.PHONY: avail-loadtest
-avail-loadtest: build ## Run loadtest against an Avail chain.
-	$(BUILD_DIR)/$(BIN_NAME) loadtest --verbosity 700 --chain-id 1256 --concurrency 1 --requests 1000 --rate-limit 5 --mode t --data-avail http://127.0.0.1:$(PORT)
