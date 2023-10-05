78c9a18949c49630d4bb562fa1bcbcc4a05c8b1c
leovct <l009.vincent@gmail.com>
2023-10-05 11:50:29 +0200

    chore: update build process

--- contracts/uniswapv3/build.sh
+++ contracts/uniswapv3/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # This script builds UniswapV3 contracts.
+set -e
 mode="${1:-all}"
 
 # Make sure the local chain is started.
@@ -29,44 +30,44 @@ build_contracts() {
 	echo -e "\n🏗️  Building $repository contracts..."
 
 	# Clone repository.
-	git clone --branch $branch $url ./tmp/$repository
+	git clone --branch $branch $url ./tmp/$repository-$branch
 
 	# Install dependencies.
-	pushd tmp/$repository
+	pushd tmp/$repository-$branch
 	yarn install
 	popd
 
 	# Update contract's array path.
 	new_array=()
 	for contract in "${contracts[@]}"; do
-		new_array+=("tmp/$repository/contracts/$contract.sol")
+		new_array+=("tmp/$repository-$branch/contracts/$contract.sol")
 	done
 
 	# Remove old artefacts.
-	rm -rf ./$repository/*
+	rm -rf ./$repository-$branch/*
 
 	# Compile contracts.
 	for element in "${new_array[@]}"; do
     echo "$element"
 	done
 	solc "${new_array[@]}" \
-		@uniswap=$current_dir/tmp/$repository/node_modules/@uniswap \
-		@openzeppelin=$current_dir/tmp/$repository/node_modules/@openzeppelin \
-		base64-sol=$current_dir/tmp/$repository/node_modules/base64-sol \
-		../interfaces=$current_dir/tmp/$repository/contracts/interfaces \
-		../libraries=$current_dir/tmp/$repository/contracts/libraries \
+		@uniswap=$current_dir/tmp/$repository-$branch/node_modules/@uniswap \
+		@openzeppelin=$current_dir/tmp/$repository-$branch/node_modules/@openzeppelin \
+		base64-sol=$current_dir/tmp/$repository-$branch/node_modules/base64-sol \
+		../interfaces=$current_dir/tmp/$repository-$branch/contracts/interfaces \
+		../libraries=$current_dir/tmp/$repository-$branch/contracts/libraries \
 		--evm-version istanbul \
 		--optimize \
 		--optimize-runs 200 \
 		--abi \
 		--bin \
-		--output-dir ./$repository \
+		--output-dir ./$repository-$branch \
 		--overwrite
 
 	# Clean up.
-	rm -rf ./tmp/$repository
+	rm -rf ./tmp/$repository-$branch
 
-	echo "✅ Successfully built $repository contracts..."
+	echo "✅ Successfully built $repository-$branch contracts..."
 }
 
 # Use solc 0.7.6.
@@ -74,20 +75,37 @@ solc-select use 0.7.6
 solc --version
 current_dir=$(pwd)
 
+# Clean up
+rm -rf ./tmp
+
 ## Build v3-core contracts.
 if [ "$mode" == "v3-core" ] || [ "$mode" == "all" ]; then
 	contracts=("UniswapV3Factory" "UniswapV3Pool")
 	build_contracts v3-core https://github.com/Uniswap/v3-core.git v1.0.0 $contracts
 fi
 
-## Build v3-periphery contracts.
+## Build v3-periphery v1.1.1 contracts
 if [ "$mode" == "v3-periphery" ] || [ "$mode" == "all" ]; then
-	contracts=("lens/UniswapInterfaceMulticall" "lens/TickLens" "libraries/NFTDescriptor" "NonfungibleTokenPositionDescriptor" "NonfungiblePositionManager" "V3Migrator")
-	build_contracts v3-periphery https://github.com/Uniswap/v3-periphery.git v1.3.0 $contracts
-	mv v3-periphery/NonfungibleTokenPositionDescriptor.abi v3-periphery/NFTPositionDescriptor.abi
-	mv v3-periphery/NonfungibleTokenPositionDescriptor.bin v3-periphery/NFTPositionDescriptor.bin
-	mv v3-periphery/NonfungiblePositionManager.abi v3-periphery/NFPositionManager.abi
-	mv v3-periphery/NonfungiblePositionManager.bin v3-periphery/NFPositionManager.bin
+	repository=v3-periphery
+	branch=v1.1.1
+	contracts=("lens/UniswapInterfaceMulticall" "lens/TickLens" "NonfungiblePositionManager" "V3Migrator")
+	build_contracts $repository https://github.com/Uniswap/v3-periphery.git $branch $contracts
+
+	# Rename the NonfungiblePositionManager contract.
+	mv $repository-$branch/NonfungiblePositionManager.abi $repository-$branch/NFPositionManager.abi
+	mv $repository-$branch/NonfungiblePositionManager.bin $repository-$branch/NFPositionManager.bin
+fi
+
+## Build v3-periphery v1.3.0 contracts
+if [ "$mode" == "v3-periphery" ] || [ "$mode" == "all" ]; then
+	repository=v3-periphery
+	branch=v1.3.0
+	contracts=("libraries/NFTDescriptor" "NonfungibleTokenPositionDescriptor")
+	build_contracts $repository https://github.com/Uniswap/v3-periphery.git $branch $contracts
+
+	# Rename the NonfungibleTokenPositionDescriptor contract.
+	mv $repository-$branch/NonfungibleTokenPositionDescriptor.abi $repository-$branch/NFTPositionDescriptor.abi
+	mv $repository-$branch/NonfungibleTokenPositionDescriptor.bin $repository-$branch/NFTPositionDescriptor.bin
 
 	# We need to deloy the NFTDescriptor library, retrieve its address and link it inside
 	# NonfungibleTokenPositionDescriptor bytecode. This is required to generate the Go binding.
@@ -98,12 +116,12 @@ if [ "$mode" == "v3-periphery" ] || [ "$mode" == "all" ]; then
 		--private-key 0x42b6e34dc21598a807dc19d7784c71b2a7a01f6480dc6f58258f78e539f1a1fa \
 		--json \
 		--create \
-		"$(cat v3-periphery/NFTDescriptor.bin)" \
+		"$(cat $repository-$branch/NFTDescriptor.bin)" \
 		| jq -r .contractAddress)
 	solc \
 		--link \
-		--libraries tmp/v3-periphery/contracts/libraries/NFTDescriptor.sol:NFTDescriptor:$nft_descriptor_lib_address \
-		v3-periphery/NonfungibleTokenPositionDescriptor.bin
+		--libraries tmp/$repository-$branch/contracts/libraries/NFTDescriptor.sol:NFTDescriptor:$nft_descriptor_lib_address \
+		$repository-$branch/NFTPositionDescriptor.bin
 	echo "✍️  NFTDescriptor library address: $nft_descriptor_lib_address"
 fi
 
@@ -114,7 +132,7 @@ if [ "$mode" == "v3-staker" ] || [ "$mode" == "all" ]; then
 fi
 
 ## Build v3-swap-router contracts.
-if [ "$mode" == "v3-swap-router" ] || [ "$mode" == "v3" ]; then
+if [ "$mode" == "v3-swap-router" ] || [ "$mode" == "all" ]; then
 	contracts=("lens/QuoterV2" "SwapRouter02")
 	build_contracts v3-swap-router https://github.com/Uniswap/swap-router-contracts.git v1.3.0 $contracts
 fi
