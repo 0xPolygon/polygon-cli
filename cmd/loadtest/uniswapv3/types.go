e81e59245b767db6af82c0e20da5c900bb212ab5
leovct <l009.vincent@gmail.com>
2023-10-19 11:44:42 +0200

    doc: document `types.go`

--- cmd/loadtest/uniswapv3/types.go
+++ cmd/loadtest/uniswapv3/types.go
@@ -6,4 +6,6 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+// blockUntilSuccessfulFn is designed to wait until a specified number of Ethereum blocks have been
+// mined, periodically checking for the completion of a given function within each block interval.
 type blockUntilSuccessfulFn func(ctx context.Context, c *ethclient.Client, f func() error) error
