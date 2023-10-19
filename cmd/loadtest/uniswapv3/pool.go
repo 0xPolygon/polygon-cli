560f4ff488faa4abd3921e152d23d11faffd41cc
leovct <l009.vincent@gmail.com>
2023-10-19 11:42:08 +0200

    chore: nit

--- cmd/loadtest/uniswapv3/pool.go
+++ cmd/loadtest/uniswapv3/pool.go
@@ -16,6 +16,7 @@ import (
 
 var (
 	// Trading fees charged on each swap or trade made within a UniswapV3 liquidity pool.
+	// https://docs.uniswap.org/concepts/protocol/fees
 	poolFees = big.NewInt(3_000)
 
 	// Reserve of a token in a UniswapV3 pool.
