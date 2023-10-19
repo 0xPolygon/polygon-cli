25ab60dd23d393cd96cbf4fc155a0f9bfadd6945
leovct <l009.vincent@gmail.com>
2023-10-19 11:32:01 +0200

    chore: document `swap.go`

--- cmd/loadtest/uniswapv3/swap.go
+++ cmd/loadtest/uniswapv3/swap.go
@@ -11,9 +11,11 @@ import (
 
 var (
 	// The amount of inbound token given as swap input.
-	swapAmountIn = big.NewInt(1000)
+	swapAmountIn = big.NewInt(1_000)
+
 	// The minimum amount of outbound token received as swap output.
-	swapAmountOutMinimum = big.NewInt(996)
+	// It is set to a percentage of `swapAmountIn` (regardless of pool fees).
+	swapAmountOutMinimum = new(big.Int).Mul(swapAmountIn, new(big.Int).Div(big.NewInt(98), big.NewInt(100)))
 )
 
 // ExactInputSingleSwap performs a UniswapV3 swap using the `ExactInputSingle` method which swaps a fixed amount of
@@ -21,23 +23,14 @@ var (
 // by the nonce value.
 func ExactInputSingleSwap(tops *bind.TransactOpts, swapRouter *uniswapv3.SwapRouter02, poolConfig PoolConfig, recipient common.Address, nonce uint64) error {
 	// Determine the direction of the swap.
-	tIn := poolConfig.Token0
-	tInName := "token0"
-	tOut := poolConfig.Token1
-	tOutName := "token1"
-	if nonce%2 == 0 {
-		tIn = poolConfig.Token1
-		tInName = "token1"
-		tOut = poolConfig.Token0
-		tOutName = "token0"
-	}
+	swapDirection := getSwapDirection(nonce, poolConfig)
 
 	// Perform swap.
 	tx, err := swapRouter.ExactInputSingle(tops, uniswapv3.IV3SwapRouterExactInputSingleParams{
 		// The contract address of the inbound token.
-		TokenIn: tIn.Address,
+		TokenIn: swapDirection.tokenIn,
 		// The contract address of the outbound token.
-		TokenOut: tOut.Address,
+		TokenOut: swapDirection.tokenOut,
 		// The fee tier of the pool, used to determine the correct pool contract in which to execute the swap.
 		Fee: poolConfig.Fees,
 		// The destination address of the outbound token.
@@ -52,10 +45,36 @@ func ExactInputSingleSwap(tops *bind.TransactOpts, swapRouter *uniswapv3.SwapRou
 		SqrtPriceLimitX96: big.NewInt(0),
 	})
 	if err != nil {
-		log.Error().Err(err).Str("tokenIn", tInName).Str("tokenOut", tOutName).Msg("Unable to swap")
+		log.Error().Err(err).Str("tokenIn", swapDirection.tokenInName).Str("tokenOut", swapDirection.tokenOutName).Msg("Unable to swap")
 		return err
 	}
-	log.Debug().Str("tokenIn", tInName).Str("tokenOut", tOutName).Msg("Successful swap")
+	log.Debug().Str("tokenIn", swapDirection.tokenInName).Str("tokenOut", swapDirection.tokenOutName).Msg("Successful swap")
 	log.Trace().Interface("hash", tx.Hash()).Msg("Transaction")
 	return nil
+}
+
+// swapDirection represents a swap direction with the inbound and outbound tokens.
+type swapDirection struct {
+	tokenIn, tokenOut         common.Address
+	tokenInName, tokenOutName string
+}
+
+// Return the direction of the swap given the nonce value.
+func getSwapDirection(nonce uint64, poolConfig PoolConfig) swapDirection {
+	if nonce%2 == 0 {
+		return swapDirection{
+			tokenIn:     poolConfig.Token0.Address,
+			tokenInName: "token0",
+
+			tokenOut:     poolConfig.Token1.Address,
+			tokenOutName: "token1",
+		}
+	}
+	return swapDirection{
+		tokenIn:     poolConfig.Token1.Address,
+		tokenInName: "token1",
+
+		tokenOut:     poolConfig.Token0.Address,
+		tokenOutName: "token0",
+	}
 }
