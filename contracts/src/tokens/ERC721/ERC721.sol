2f2c46db521a57c6e83f2cebd342af7009943579
leovct <l009.vincent@gmail.com>
2023-11-07 14:12:51 +0100

    fix: issue with `ERC721`

--- contracts/src/tokens/ERC721/ERC721.sol
+++ contracts/src/tokens/ERC721/ERC721.sol
@@ -4,13 +4,16 @@ pragma solidity ^0.8.21;
 import {ERC721 as OZ_ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract ERC721 is OZ_ERC721 {
+    uint256 public currentTokenId = 0;
+
     constructor() OZ_ERC721("MyNFT", "MNFT") {
         mintBatch(msg.sender, 1000);
     }
 
     function mintBatch(address to, uint256 amount) public {
-        for (uint256 i = 1; i <= amount; i++) {
-            _safeMint(to, i);
+       for (uint256 i = 0; i < amount; i++) {
+            uint256 newTokenId = ++currentTokenId;
+            _safeMint(to, newTokenId);
         }
     }
 }
