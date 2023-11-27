c1cf6af471050c2280fe9327160ba16468d69d8b
leovct <l009.vincent@gmail.com>
2023-11-27 16:48:29 +0100

    chore: update `Funder`

--- contracts/src/funder/Funder.sol
+++ contracts/src/funder/Funder.sol
@@ -17,21 +17,21 @@ contract Funder {
   }
 
   /// @notice Fund a specific account with the predefined funding amount.
-  /// @param account The address of the account to be funded.
-  function fundAccount(address account) public {
-    require(account != address(0), "The funded address should be different thant the zero address");
+  /// @param _account The address of the account to be funded.
+  function fund(address _account) public {
+    require(_account != address(0), "The funded address should be different thant the zero address");
     require(address(this).balance >= amount, "Insufficient contract balance for funding");
 
-    (bool success, ) = account.call{value: amount}("");
+    (bool success, ) = _account.call{value: amount}("");
     require(success, "Funding failed");
   }
 
   /// @notice Fund multiple accounts with the predefined funding amount.
-  /// @param accounts The addresses of the accounts to be funded.
-  function fundAccounts(address[] calldata accounts) external {
-    require(address(this).balance >= amount * accounts.length, "Insufficient contract balance for batch funding");
-    for (uint256 i = 0; i < accounts.length; i++) {
-      fundAccount(accounts[i]);
+  /// @param _accounts The addresses of the accounts to be funded.
+  function bulkFund(address[] calldata _accounts) external {
+    require(address(this).balance >= amount * _accounts.length, "Insufficient contract balance for batch funding");
+    for (uint256 i = 0; i < _accounts.length; i++) {
+      fund(_accounts[i]);
     }
   }
 
