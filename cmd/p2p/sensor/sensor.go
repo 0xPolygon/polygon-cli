3ba633420d189dfa799ff687b79cabfa47962b12
minhd-vu <minhd_vu@yahoo.com>
2024-07-19 13:17:38 -0400

    only get method

--- cmd/p2p/sensor/sensor.go
+++ cmd/p2p/sensor/sensor.go
@@ -292,6 +292,11 @@ func handlePrometheus() {
 
 func handlePeers(server *ethp2p.Server) {
 	http.HandleFunc("/peers", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 
