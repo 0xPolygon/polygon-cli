1706f58b3c841a4189c5ac5f39895540756a361a
John Hilliard <praetoriansentry@gmail.com>
2024-08-22 17:07:55 -0400

    feat: adding some types

--- cmd/retest/retest.go
+++ cmd/retest/retest.go
@@ -18,79 +18,85 @@ var (
 )
 
 type EthTestEnv struct {
-	CurrentBaseFee       any `json:"currentBaseFee"`
-	CurrentCoinbase      any `json:"currentCoinbase"`
-	CurrentDifficulty    any `json:"currentDifficulty"`
-	CurrentExcessBlobGas any `json:"currentExcessBlobGas"`
-	CurrentGasLimit      any `json:"currentGasLimit"`
-	CurrentNumber        any `json:"currentNumber"`
-	CurrentRandom        any `json:"currentRandom"`
-	CurrentTimestamp     any `json:"currentTimestamp"`
+	CurrentBaseFee       EthTestNumeric `json:"currentBaseFee"`
+	CurrentCoinbase      EthTestAddress `json:"currentCoinbase"`
+	CurrentDifficulty    EthTestNumeric `json:"currentDifficulty"`
+	CurrentExcessBlobGas EthTestNumeric `json:"currentExcessBlobGas"`
+	CurrentGasLimit      EthTestNumeric `json:"currentGasLimit"`
+	CurrentNumber        EthTestNumeric `json:"currentNumber"`
+	CurrentRandom        EthTestNumeric `json:"currentRandom"`
+	CurrentTimestamp     EthTestNumeric `json:"currentTimestamp"`
+}
+type EthTestAccessList struct {
+	Address     EthTestAddress   `json:"address"`
+	StorageKeys []EthTestNumeric `json:"storageKeys"`
 }
 type EthTestTx struct {
-	AccessList           any `json:"accessList"`
-	BlobVersionedHashes  any `json:"blobVersionedHashes"`
-	ChainID              any `json:"chainId"`
-	Data                 any `json:"data"`
-	ExpectException      any `json:"expectException"`
-	GasLimit             any `json:"gasLimit"`
-	GasPrice             any `json:"gasPrice"`
-	MaxFeePerBlobGas     any `json:"maxFeePerBlobGas"`
-	MaxFeePerGas         any `json:"maxFeePerGas"`
-	MaxPriorityFeePerGas any `json:"maxPriorityFeePerGas"`
-	Nonce                any `json:"nonce"`
-	R                    any `json:"r"`
-	S                    any `json:"s"`
-	SecretKey            any `json:"secretKey"`
-	To                   any `json:"to"`
-	V                    any `json:"v"`
-	Value                any `json:"value"`
+	AccessList          []EthTestAccessList `json:"accessList"`
+	BlobVersionedHashes []EthTestHash       `json:"blobVersionedHashes"`
+	ChainID             EthTestNumeric      `json:"chainId"`
+	Data                EthTestData         `json:"data"`
+
+	GasLimit             EthTestNumeric `json:"gasLimit"`
+	GasPrice             EthTestNumeric `json:"gasPrice"`
+	MaxFeePerBlobGas     EthTestNumeric `json:"maxFeePerBlobGas"`
+	MaxFeePerGas         EthTestNumeric `json:"maxFeePerGas"`
+	MaxPriorityFeePerGas EthTestNumeric `json:"maxPriorityFeePerGas"`
+	Nonce                EthTestNumeric `json:"nonce"`
+	R                    EthTestNumeric `json:"r"`
+	S                    EthTestNumeric `json:"s"`
+	To                   EthTestAddress `json:"to"`
+	V                    EthTestNumeric `json:"v"`
+	Value                EthTestNumeric `json:"value"`
+	// Unused
+	ExpectException any `json:"expectException"`
+	SecretKey       any `json:"secretKey"`
 }
 type EthTestGenesis struct {
-	BaseFeePerGas         any `json:"baseFeePerGas"`
-	BeaconRoot            any `json:"beaconRoot"`
-	BlobGasUsed           any `json:"blobGasUsed"`
-	Bloom                 any `json:"bloom"`
-	Coinbase              any `json:"coinbase"`
-	Difficulty            any `json:"difficulty"`
-	ExcessBlobGas         any `json:"excessBlobGas"`
-	ExtraData             any `json:"extraData"`
-	GasLimit              any `json:"gasLimit"`
-	GasUsed               any `json:"gasUsed"`
-	Hash                  any `json:"hash"`
-	MixHash               any `json:"mixHash"`
-	Nonce                 any `json:"nonce"`
-	Number                any `json:"number"`
-	ParentBeaconBlockRoot any `json:"parentBeaconBlockRoot"`
-	ParentHash            any `json:"parentHash"`
-	ReceiptTrie           any `json:"receiptTrie"`
-	StateRoot             any `json:"stateRoot"`
-	Timestamp             any `json:"timestamp"`
-	TransactionsTrie      any `json:"transactionsTrie"`
-	UncleHash             any `json:"uncleHash"`
-	WithdrawalsRoot       any `json:"withdrawalsRoot"`
+	BaseFeePerGas         EthTestNumeric `json:"baseFeePerGas"`
+	BeaconRoot            EthTestHash    `json:"beaconRoot"`
+	BlobGasUsed           EthTestNumeric `json:"blobGasUsed"`
+	Bloom                 EthTestData    `json:"bloom"`
+	Coinbase              EthTestAddress `json:"coinbase"`
+	Difficulty            EthTestNumeric `json:"difficulty"`
+	ExcessBlobGas         EthTestNumeric `json:"excessBlobGas"`
+	ExtraData             EthTestData    `json:"extraData"`
+	GasLimit              EthTestNumeric `json:"gasLimit"`
+	GasUsed               EthTestNumeric `json:"gasUsed"`
+	Hash                  EthTestHash    `json:"hash"`
+	MixHash               EthTestHash    `json:"mixHash"`
+	Nonce                 EthTestNumeric `json:"nonce"`
+	Number                EthTestNumeric `json:"number"`
+	ParentBeaconBlockRoot EthTestHash    `json:"parentBeaconBlockRoot"`
+	ParentHash            EthTestHash    `json:"parentHash"`
+	ReceiptTrie           EthTestHash    `json:"receiptTrie"`
+	StateRoot             EthTestHash    `json:"stateRoot"`
+	Timestamp             EthTestNumeric `json:"timestamp"`
+	TransactionsTrie      EthTestHash    `json:"transactionsTrie"`
+	UncleHash             EthTestHash    `json:"uncleHash"`
+	WithdrawalsRoot       EthTestHash    `json:"withdrawalsRoot"`
 }
 type EthTestBlocks struct {
 	Transactions []EthTestTx `json:"transactions"`
 }
 
 type EthTestPre struct {
-	Nonce   any `json:"nonce"`
-	Balance any `json:"balance"`
-	Storage any `json:"storage"`
-	Code    any `json:"code"`
+	Nonce   EthTestNumeric            `json:"nonce"`
+	Balance EthTestNumeric            `json:"balance"`
+	Storage map[string]EthTestNumeric `json:"storage"`
+	Code    EthTestData               `json:"code"`
 }
 
 // This is based on examination of the data. We've left out the fields at are used in less then 10 tests
 type EthTest struct {
 	Pre                map[string]EthTestPre `json:"pre"`                // Pre determines preallocated accounts in the test case
-	Expect             any                   `json:"expect"`             // Expect are the success conditions. In our cases we won't be able to validate them
 	Transaction        EthTestTx             `json:"transaction"`        // Transaction is usually a test transaction
 	Env                EthTestEnv            `json:"env"`                // Env sets the environment for execution. This is not relvant usually for creating load
-	Info               any                   `json:"_info"`              // Info mostly comments
 	GenesisBlockHeader EthTestGenesis        `json:"genesisBlockHeader"` // GenesisBlockHeader sets the typical genesis params of the network.
 	Blocks             []EthTestBlocks       `json:"blocks"`             // Blocks are the blocks that lead to the state being tested
 	SealEngine         string                `json:"sealEngine"`         // SealEngine is either Null or NoProof
+	Expect             any                   `json:"expect"`             // Expect are the success conditions. In our cases we won't be able to validate them
+	Info               any                   `json:"_info"`              // Info mostly comments
 	ExpectException    any                   `json:"expectException"`    // ExpectException also determins if errors are expected for certain levels of HF
 	Vectors            any                   `json:"vectors"`            // Vectors are specific cases which we probably won't use
 	TxBytes            any                   `json:"txbytes"`            // TxBytes are RLP tests to send directly without manipulation
@@ -100,6 +106,11 @@ type EthTest struct {
 }
 
 type EthTestSuite map[string]EthTest
+
+type EthTestNumeric any
+type EthTestData any
+type EthTestHash any
+type EthTestAddress any
 
 func decodeToUint64(v any) (uint64, error) {
 	switch v := reflect.ValueOf(v); v.Kind() {
