913e874f549ef0c78d271982a60188d5db901f03
John Hilliard <praetoriansentry@gmail.com>
2024-08-22 19:42:25 -0400

    feat: wip parsing

--- cmd/retest/retest.go
+++ cmd/retest/retest.go
@@ -7,14 +7,22 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"io"
+	"math"
+	"math/big"
 	"os"
 	"reflect"
+	"regexp"
+	"strings"
 )
 
 var (
 	//go:embed usage.md
 	usage         string
 	inputFileName *string
+
+	validBase10    *regexp.Regexp
+	dataLabel      *regexp.Regexp
+	typeIndidcator *regexp.Regexp
 )
 
 type EthTestEnv struct {
@@ -32,22 +40,21 @@ type EthTestAccessList struct {
 	StorageKeys []EthTestNumeric `json:"storageKeys"`
 }
 type EthTestTx struct {
-	AccessList          []EthTestAccessList `json:"accessList"`
-	BlobVersionedHashes []EthTestHash       `json:"blobVersionedHashes"`
-	ChainID             EthTestNumeric      `json:"chainId"`
-	Data                EthTestData         `json:"data"`
-
-	GasLimit             EthTestNumeric `json:"gasLimit"`
-	GasPrice             EthTestNumeric `json:"gasPrice"`
-	MaxFeePerBlobGas     EthTestNumeric `json:"maxFeePerBlobGas"`
-	MaxFeePerGas         EthTestNumeric `json:"maxFeePerGas"`
-	MaxPriorityFeePerGas EthTestNumeric `json:"maxPriorityFeePerGas"`
-	Nonce                EthTestNumeric `json:"nonce"`
-	R                    EthTestNumeric `json:"r"`
-	S                    EthTestNumeric `json:"s"`
-	To                   EthTestAddress `json:"to"`
-	V                    EthTestNumeric `json:"v"`
-	Value                EthTestNumeric `json:"value"`
+	AccessList           []EthTestAccessList `json:"accessList"`
+	BlobVersionedHashes  []EthTestHash       `json:"blobVersionedHashes"`
+	ChainID              EthTestNumeric      `json:"chainId"`
+	Data                 EthTestData         `json:"data"`
+	GasLimit             EthTestNumeric      `json:"gasLimit"`
+	GasPrice             EthTestNumeric      `json:"gasPrice"`
+	MaxFeePerBlobGas     EthTestNumeric      `json:"maxFeePerBlobGas"`
+	MaxFeePerGas         EthTestNumeric      `json:"maxFeePerGas"`
+	MaxPriorityFeePerGas EthTestNumeric      `json:"maxPriorityFeePerGas"`
+	Nonce                EthTestNumeric      `json:"nonce"`
+	R                    EthTestNumeric      `json:"r"`
+	S                    EthTestNumeric      `json:"s"`
+	To                   EthTestAddress      `json:"to"`
+	V                    EthTestNumeric      `json:"v"`
+	Value                EthTestNumeric      `json:"value"`
 	// Unused
 	ExpectException any `json:"expectException"`
 	SecretKey       any `json:"secretKey"`
@@ -87,7 +94,7 @@ type EthTestPre struct {
 	Code    EthTestData               `json:"code"`
 }
 
-// This is based on examination of the data. We've left out the fields at are used in less then 10 tests
+// EthTest is based on examination of the data. We've left out the fields at are used in less then 10 tests
 type EthTest struct {
 	Pre                map[string]EthTestPre `json:"pre"`                // Pre determines preallocated accounts in the test case
 	Transaction        EthTestTx             `json:"transaction"`        // Transaction is usually a test transaction
@@ -112,18 +119,159 @@ type EthTestData any
 type EthTestHash any
 type EthTestAddress any
 
-func decodeToUint64(v any) (uint64, error) {
-	switch v := reflect.ValueOf(v); v.Kind() {
-	case reflect.Float64:
-		// fmt.Println(v.Int())
-	case reflect.String:
-	// 0x0abc123
-	case reflect.Slice:
-		// idk
-	default:
-		fmt.Printf("unhandled kind %s\n", v.Kind())
+type WrappedNumeric struct {
+	raw   EthTestNumeric
+	inner *big.Int
+}
+
+type WrappedData struct {
+	raw   EthTestData
+	inner []byte
+}
+
+func (wr *WrappedNumeric) ToBigInt() *big.Int {
+	if wr.inner != nil {
+		return wr.inner
 	}
-	return 0, nil
+	wr.inner = EthTestNumericToBigInt(wr.raw)
+	return wr.inner
+}
+
+func EthTestNumericToBigInt(num EthTestNumeric) *big.Int {
+	if num == nil {
+		return nil
+	}
+	v := reflect.ValueOf(num)
+	if v.IsZero() {
+		return nil
+	}
+
+	switch v.Kind() {
+	case reflect.Float64:
+		return float64ToBigInt(v.Float())
+	case reflect.String:
+		return processNumericString(v.String())
+	case reflect.Slice:
+		if v.Len() == 0 {
+			log.Warn().Msg("The slice is empty; returning nil")
+			return nil
+		}
+		first := v.Index(0)
+		// TODO this indicates this is a matrixed parameter.. WE're losing out right now
+		log.Debug().Any("first", first).Msg("A numeric field is multi-valued. This isn't currently supported and we'll use the first element")
+		return EthTestNumericToBigInt(first.Interface().(EthTestNumeric))
+	default:
+		log.Fatal().Any("input", num).Str("kind", v.Kind().String()).Msg("Attempted to convert unknown type to number")
+	}
+	return new(big.Int)
+}
+func float64ToBigInt(f float64) *big.Int {
+	// Step 1: Round the float64 (e.g., using math.Round)
+	roundedFloat := math.Round(f)
+
+	// Step 2: Convert the rounded float to a string
+	str := fmt.Sprintf("%.0f", roundedFloat)
+
+	// Step 3: Create a big.Int and set it from the string
+	bigInt := new(big.Int)
+	bigInt.SetString(str, 10)
+
+	return bigInt
+}
+
+func processNumericString(s string) *big.Int {
+	log.Trace().Str("numString", s).Msg("Converting numeric string to big int")
+	s = strings.TrimSpace(s)
+	s = strings.TrimPrefix(s, "0x:bigint ")
+	if strings.Contains(s, ":") {
+		log.Fatal().Str("string", s).Msg("Unknown number format")
+	}
+	if strings.HasPrefix(s, "0x") {
+		s = strings.TrimPrefix(s, "0x")
+		num, fullRead := new(big.Int).SetString(s, 16)
+		if !fullRead {
+			log.Fatal().Str("input", s).Msg("Unable to read the full hex data?!")
+		}
+		return num
+	}
+
+	if validBase10.MatchString(s) {
+		num, fullRead := new(big.Int).SetString(s, 10)
+		if !fullRead {
+			log.Fatal().Str("input", s).Msg("Unable to read the full numeric data")
+		}
+		return num
+	}
+	num, fullRead := new(big.Int).SetString(s, 16)
+	if !fullRead {
+		log.Fatal().Str("input", s).Msg("Unable to read the full numeric data")
+	}
+	return num
+}
+
+func (wd *WrappedData) ToBytes() []byte {
+	if wd.inner != nil {
+		return wd.inner
+	}
+	wd.inner = EthTestDataToBytes(wd.raw)
+	return nil
+}
+func EthTestDataToBytes(data EthTestData) []byte {
+	if data == nil {
+		return nil
+	}
+	v := reflect.ValueOf(data)
+	switch v.Kind() {
+	case reflect.String:
+		return processTestDataString(v.String())
+	case reflect.Map:
+		dataMap := data.(map[string]any)
+		_, hasData := dataMap["data"]
+		_, hasAccessList := dataMap["accessList"]
+		if !hasData || !hasAccessList {
+			log.Fatal().Msg("Got a data field with a map type that wasn't data + access list")
+		}
+		// TODO - we're losing the ability to send lots of differenth access list tests
+		return EthTestDataToBytes(dataMap["data"])
+	case reflect.Slice:
+		if v.Len() == 0 {
+			log.Warn().Msg("The slice is empty; returning nil")
+			return nil
+		}
+		// TODO these tests should be collected somehow
+		for i := 0; i < v.Len(); i = i + 1 {
+			EthTestDataToBytes(v.Index(i).Interface().(EthTestData))
+		}
+		return EthTestDataToBytes(v.Index(0).Interface().(EthTestData))
+	default:
+		log.Fatal().Any("input", data).Str("kind", v.Kind().String()).Msg("Attempted to convert unknown type to raw data")
+	}
+	return nil
+}
+
+func processTestDataString(data string) []byte {
+	data = strings.TrimSpace(data)
+	if dataLabel.MatchString(data) {
+		label := dataLabel.FindStringSubmatch(data)
+		data = dataLabel.ReplaceAllString(data, "")
+		log.Trace().Str("label", label[1]).Msg("stripping label")
+	}
+	data = strings.TrimSpace(data)
+	if data == "" {
+		return nil
+	}
+	if typeIndidcator.MatchString(data) {
+		rawType := typeIndidcator.FindStringSubmatch(data)[1]
+		// raw
+		// abi
+		// yul
+		fmt.Println(rawType)
+	} else {
+		// LLL (I think)
+		fmt.Println(data)
+	}
+
+	return nil
 }
 
 var RetestCmd = &cobra.Command{
@@ -143,6 +291,14 @@ var RetestCmd = &cobra.Command{
 		}
 		for k := range tests {
 			log.Debug().Str("testname", k).Msg("Parsing test")
+			n := tests[k].Transaction.GasLimit
+			wn := WrappedNumeric{raw: n}
+			if wn.ToBigInt() != nil {
+				log.Trace().Uint64("nonce", wn.ToBigInt().Uint64()).Msg("Parsing nonce")
+			}
+			d := tests[k].Transaction.Data
+			wd := WrappedData{raw: d}
+			wd.ToBytes()
 		}
 		return nil
 	},
@@ -154,6 +310,10 @@ var RetestCmd = &cobra.Command{
 func init() {
 	flagSet := RetestCmd.PersistentFlags()
 	inputFileName = flagSet.String("file", "", "Provide a file that's filed with test transaction fillers")
+
+	validBase10 = regexp.MustCompile(`^[0-9]*$`)
+	dataLabel = regexp.MustCompile(`^:label ([^ ]*) `)
+	typeIndidcator = regexp.MustCompile(`^:([^ ]*) `)
 }
 
 func getInputData(cmd *cobra.Command, args []string) ([]byte, error) {
