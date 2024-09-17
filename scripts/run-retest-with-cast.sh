b715bb6ef1c7cf40120f9e18d97a5cceaf1d1e4b
John Hilliard <praetoriansentry@gmail.com>
2024-09-17 09:06:04 -0400

    feat: latest tweaks to the run retest script

--- scripts/run-retest-with-cast.sh
+++ scripts/run-retest-with-cast.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 # ./scripts/run-retest-with-cast.sh < simple-test-out-new.json 2>&1 | tee -a local-test-aug-28-2.logs
-# find /tmp -type f -newer /tmp/.retest-resume-465de2ded091399032b8632153d8a568bad397718d512517993f12d1eb856c64 -name '.retest-resume-*' | xargs rm
+# find /tmp -type f -newer /tmp/.retest-resume-d1c13e3e28572fa775ccc4df25307913069300677c6745f9df8b739dd833c236 -name '.retest-resume-*' | xargs rm
 
-private_key="0x12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df82c625"
-rpc_url="http://127.0.0.1:33686"
+private_key="0x12d7de8621a77640c9241b2595ba78ce443d05e94090365ab3bb5e19df82c625" # kurtosis
+rpc_url=$(kurtosis port print erigon06 sequencer001 sequencer8123)
+eth_address=$(cast wallet address --private-key $private_key)
+tester_addr=0x1E8422B74Ead154312B96785602675442755B373
+test_actions="0001 0002 0003 0004 0011 0012 0013 0014 0021 0022 0023 0024 0031 0032 0033 0034 0041 0042 0043 0044 0101 0201 0301 0401 0501 0601 0701 0801 0901 1011 1012 1013 1014 2011 2012 2013 2014"
 
+# Nonce Purge
+# polycli loadtest --mode t --private-key $private_key --requests 10 --rate-limit 500 --verbosity 700 --rpc-url $rpc_url --legacy --gas-limit 21000 --gas-price 2000000000 --concurrency 250 --eth-amount 0.000000000000000001
 
-legacy_flag=" --legacy "
+legacy_flag=" --legacy --gas-price 3000000000 "
+# legacy_flag=""
 clean_up="true"
 gas_limit=1000000
 
 function print_warning() {
-    2>&1 echo -e "\e[41m\e[97m$1\e[0m"
+    >&2 echo -e "\e[41m\e[97m$1\e[0m"
 }
 
 function normalize_address() {
-        sed 's/0x//' |
-         tr '[:upper:]' '[:lower:]'
+    sed 's/0x//' |
+        tr '[:upper:]' '[:lower:]'
 }
 
 function hex_to_dec() {
@@ -40,43 +46,21 @@ function mark_progress() {
 
     bn=$(cast block-number --rpc-url "$rpc_url")
     cur_time=$(date -R)
-    2>&1 printf "\n\n"
-    2>&1 echo "################################################################################"
-    2>&1 echo "Starting test #$test_counter $test_name at block $bn at $cur_time"
-    2>&1 echo "Test source $test_file with test lock /tmp/.retest-resume-$test_hash"
-    2>&1 echo "################################################################################"
+    >&2 printf "\n\n"
+    >&2 echo "################################################################################"
+    >&2 echo "Starting test #$test_counter $test_name at block $bn at $cur_time"
+    >&2 echo "Test source $test_file with test lock /tmp/.retest-resume-$test_hash"
+    >&2 echo "################################################################################"
 }
 
-# We'll attempt to send a synchronous transaction. If that works
-# (doesn't time out), it means that the test is no longer pending. If
-# it failed, it means that we might need to clear out some pending
-# transactions. If we don't do this, it's very easy for one test to
-# interfere with the execution of the next test.
-function clear_pending_txs() {
-    local last_nonce
-    local current_nonce
 
-    last_nonce=$1
-    # shellcheck disable=SC2086
-    timeout 30 cast send $legacy_flag --nonce "$(cat last.nonce)" --rpc-url "$rpc_url" --private-key "$private_key" --value 1 "$wallet_address"
-    ret_code=$?
+function increment_nonce() {
+    # Lock the file, update the nonce, and unlock
+    nonce_file=$1
 
-    if [[ $ret_code -eq 0 ]]; then
-        return 0
-    fi
-
-    print_warning "The transaction to clear pending txs is stuck.. attemping to clear all stuck transaction. This means the previous test did not execute properly"
-    current_nonce=$(cast nonce --rpc-url "$rpc_url" "$wallet_address")
-    print_warning "Attemping relacements from $current_nonce to $last_nonce"
-    gp=$(cast gas-price --rpc-url $rpc_url)
-    gp=$(bc <<< "2 * $gp")
-
-    for ((i = current_nonce ; i <= last_nonce ; i++)); do
-        # shellcheck disable=SC2086
-        cast send $legacy_flag --nonce "$i" --gas-price $gp --rpc-url "$rpc_url" --private-key "$private_key" --value 1 "$wallet_address"
-    done
-
-    return 1
+    nonce=$(flock "$nonce_file" -c "nonce=\$(cat $nonce_file); echo \$((nonce + 1)) > $nonce_file; echo \$nonce")
+    echo $nonce
+    >&2 echo "INCREMENTED NONCE: $nonce"
 }
 
 function process_test_item() {
@@ -92,11 +76,6 @@ function process_test_item() {
     test_counter=$2
 
     test_hash=$(sha256sum "$testfile" | sed 's/ .*//')
-    if [[ -e "/tmp/.retest-resume-$test_hash" ]]; then
-        2>&1 echo "it looks like we have already tested this case. Skipping"
-        return
-    fi
-
     touch "/tmp/.retest-resume-$test_hash"
 
     test_name=$(jq -r '.testCases[0].name' "$testfile")
@@ -105,7 +84,6 @@ function process_test_item() {
     tmp_dir=$(mktemp -p /tmp -d retest-work-XXXXXXXXXXXX)
     pushd "$tmp_dir" || exit 1
 
-    nonce=$(cast nonce --rpc-url "$rpc_url" "$wallet_address")
     count=0
     jq -c '.dependencies[]' "$testfile" | while read -r pre ; do
         local reference_address
@@ -115,22 +93,25 @@ function process_test_item() {
         echo "$pre" | jq '.' > "dep-$count.json"
         code_to_deploy=$(jq -r '.code' "dep-$count.json")
         reference_address=$(jq -r '.addr' "dep-$count.json" | normalize_address)
-        2>&1 echo "deploying dependency $count for $reference_address"
-        2>&1 echo "current nonce: $nonce"
+        >&2 echo "deploying dependency $count for $reference_address"
+        nonce=$(increment_nonce $nonce_tmp_file)
+        >&2 echo "current nonce: $nonce"
+
+        set -x
         # shellcheck disable=SC2086
-        cast send $legacy_flag: --async --nonce "$nonce" --rpc-url "$rpc_url" --private-key "$private_key" --create "$code_to_deploy" | tee "$reference_address.txhash"
+        cast send $legacy_flag --async --nonce "$nonce" --rpc-url "$rpc_url" --private-key "$private_key" --create "$code_to_deploy" | tee "$reference_address.txhash"
+        set +x
         cast compute-address --nonce "$nonce" "$wallet_address" | sed 's/^.*0x/0x/' > "$reference_address.actual"
 
-        nonce=$((nonce+1))
-        echo "$nonce" > last.nonce
+        # this particular call is not part of the typical retest execution. But since we're not entirely accurate, i think it makes sense to do some heavy calls to the dependencies with random inputs
+        cast send $legacy_flag --async --nonce $(increment_nonce $nonce_tmp_file) --rpc-url $rpc_url --value 10 --gas-limit 200000 --private-key $private_key $(cat $reference_address.actual | sed 's/0x//')
+        for test_action in $test_actions ; do
+            cast send $legacy_flag --async --nonce $(increment_nonce $nonce_tmp_file) --rpc-url $rpc_url --value 10 --gas-limit 200000 --private-key $private_key $tester_addr \
+                "000000000000000000000000000000000000000000000000000000000000"$test_action"000000000000000000000000"$(cat $reference_address.actual | sed 's/0x//')"000000000000000000000000000000000000000000000000000000000000000F"
+        done
     done
 
-    if [[ -e last.nonce ]]; then
-        clear_pending_txs "$(cat last.nonce)"
-        rm last.nonce
-    fi
 
-    nonce=$(cast nonce --rpc-url "$rpc_url" "$wallet_address")
     count=0
     jq -c '.testCases[]' "$testfile" | while read -r "test_case" ; do
         local name
@@ -178,8 +159,9 @@ function process_test_item() {
             to_addr_arg=" $resolved_address "
         fi
 
-        2>&1 echo "executing tx $count for $name to alias of $addr"
-        2>&1 echo "current nonce: $nonce"
+        >&2 echo "executing tx $count for $name to alias of $addr"
+        nonce=$(increment_nonce $nonce_tmp_file)
+        >&2 echo "current nonce: $nonce"
 
         set -x
         # shellcheck disable=SC2086
@@ -189,19 +171,10 @@ function process_test_item() {
         if [[ $ret_code -ne 0 ]]; then
             print_warning "it looks like this request timed out.. it might be worth checking?!"
         fi
-        nonce=$((nonce+1))
-        echo "$nonce" > last.nonce
     done
 
-    if [[ -e last.nonce ]]; then
-        clear_pending_txs "$(cat last.nonce)"
-        ret_code=$?
-
-        if [[ $ret_code -ne 0 ]]; then
-            print_warning "Some number of test transactions were replaced due to pool failures. Please review test number $test_counter named $test_name in $testfile"
-        fi
-        rm last.nonce
-    fi
+    # avoid creating too any jobs by sending a synchronous transaction. This is deliberate not configured with a timeout, it should block until the nonce is cleared
+    cast send $legacy_flag --nonce "$(increment_nonce $nonce_tmp_file)" --rpc-url "$rpc_url" --private-key "$private_key" --value 1 "$wallet_address"
 
     popd || exit 1
 
@@ -211,17 +184,89 @@ function process_test_item() {
     fi
 }
 
+function fill_nonce_holes() {
+    local last_block_number=""
+    local current_block_number=""
+    while true; do
+        sleep 1
+        if [[ ! -e $nonce_loop_file ]]; then
+            break
+        fi
+        current_block_number=$(cast block-number --rpc-url $rpc_url)
+        if [[ $last_block_number -ne $current_block_number ]]; then
+            last_block_number=$current_block_number
+            tx_count=$(cast block --rpc-url $rpc_url -j | jq '.transactions | length')
+            if [[ $tx_count == "0" ]]; then
+                peeked_nonce=$(<$nonce_tmp_file)
+                current_nonce=$(cast nonce --rpc-url $rpc_url "$wallet_address")
+                gap=$((peeked_nonce-current_nonce))
+                if [[ $gap -gt 1 ]]; then
+                    echo "There is some nonce gap"
+                    polycli loadtest \
+                            --mode t \
+                            --private-key $private_key \
+                            --requests $gap \
+                            --rate-limit 500 \
+                            --verbosity 700 \
+                            --rpc-url $rpc_url \
+                            --legacy \
+                            --gas-limit 21000 \
+                            --gas-price 1500000000 \
+                            --concurrency 1 \
+                            --eth-amount 0.000000000000000001
+                fi
+            fi
+        fi
+    done
+}
+
 wallet_address=$(cast wallet address --private-key "$private_key")
 wallet_balance=$(cast balance --rpc-url $rpc_url "$wallet_address")
 wallet_nonce=$(cast nonce --rpc-url $rpc_url "$wallet_address")
-2>&1 echo "Address $wallet_address has a balance of $wallet_balance and nonce $wallet_nonce"
+nonce_tmp_file=$(mktemp -p /tmp retest-nonce-XXXXXXXXXXXX)
+nonce_loop_file=$(mktemp -p /tmp retest-nonce-check-XXXXXXXXXXXX)
+
+echo $wallet_nonce > $nonce_tmp_file
+>&2 echo "Address $wallet_address has a balance of $wallet_balance and nonce $wallet_nonce"
+
+touch $nonce_loop_file
+
+trap "rm $nonce_loop_file; exit" SIGINT
+
+fill_nonce_holes &
 
 test_counter=0
 # Break down each test into different files
-jq -c '.[]' | while read -r test_item ; do
+jq -c '.[]' | shuf | while read -r test_item ; do
     testfile=$(mktemp -p /tmp retest-item-jq-XXXXXXXXXXXX)
     echo "$test_item" > "$testfile"
     test_counter=$((test_counter+1))
-    process_test_item "$testfile" "$test_counter"
+
+    test_hash=$(sha256sum "$testfile" | sed 's/ .*//')
+    if [[ -e "/tmp/.retest-resume-$test_hash" ]]; then
+        >&2 echo "it looks like we have already tested this case. Skipping"
+        continue
+    fi
+
+    log_file="$(mktemp -p /tmp process-log-XXXXXXXX)"
+
+    # Run the test in the background and redirect its output to the log file
+    (
+        process_test_item "$testfile" "$test_counter" &> "$log_file"
+        dt=$(date -Is)
+        awk -v pid=$$ -v dt=$dt '{print dt "\t" pid "\t" $0}' "$log_file" >&2
+        rm $log_file
+    ) &
+
+    if [[ $(jobs -r -p | wc -l) -ge 7 ]]; then
+        echo "waiting for some tests to finish"
+        wait -n  # Wait for any job to finish before continuing
+    fi
 done
 
+
+rm $nonce_loop_file
+
+sleep 30
+# Nonce Purge
+polycli loadtest --mode t --private-key $private_key --requests 10 --rate-limit 500 --verbosity 700 --rpc-url $rpc_url --legacy --gas-limit 21000 --gas-price 2000000000 --concurrency 250
