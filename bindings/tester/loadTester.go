8371d7e503af943ed4d1777ccd508a66aa5b578b
Victor Castell <0x@vcastellm.xyz>
2024-10-17 12:20:58 +0200

    chore: gen proto

--- bindings/tester/loadTester.go
+++ bindings/tester/loadTester.go
@@ -26,7 +26,6 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
-	_ = abi.ConvertType
 )
 
 // LoadTesterMetaData contains all meta data concerning the LoadTester contract.
@@ -157,11 +156,11 @@ func NewLoadTesterFilterer(address common.Address, filterer bind.ContractFiltere
 
 // bindLoadTester binds a generic wrapper to an already deployed contract.
 func bindLoadTester(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := LoadTesterMetaData.GetAbi()
+	parsed, err := abi.JSON(strings.NewReader(LoadTesterABI))
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
