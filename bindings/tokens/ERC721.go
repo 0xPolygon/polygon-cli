162589d4ed2b8da63092826afa4895bc2345080e
leovct <l009.vincent@gmail.com>
2024-10-20 14:36:34 +0200

    chore: make gen

--- bindings/tokens/ERC721.go
+++ bindings/tokens/ERC721.go
@@ -26,6 +26,7 @@ var (
 	_ = common.Big1
 	_ = types.BloomLookup
 	_ = event.NewSubscription
+	_ = abi.ConvertType
 )
 
 // ERC721MetaData contains all meta data concerning the ERC721 contract.
@@ -156,11 +157,11 @@ func NewERC721Filterer(address common.Address, filterer bind.ContractFilterer) (
 
 // bindERC721 binds a generic wrapper to an already deployed contract.
 func bindERC721(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
-	parsed, err := abi.JSON(strings.NewReader(ERC721ABI))
+	parsed, err := ERC721MetaData.GetAbi()
 	if err != nil {
 		return nil, err
 	}
-	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+	return bind.NewBoundContract(address, *parsed, caller, transactor, filterer), nil
 }
 
 // Call invokes the (constant) contract method with params as input values and
