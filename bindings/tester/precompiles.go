eaebade4530f26acca8abf0425f6eb9239adae52
jason.huang <20609724+doutv@users.noreply.github.com>
2024-11-13 15:36:16 +0800

    delete unused comment

--- bindings/tester/precompiles.go
+++ bindings/tester/precompiles.go
@@ -163,24 +163,6 @@ func GenerateBlake2FInput() []byte {
 }
 
 func GenerateP256VerifyInput() []byte {
-	// message := []byte("Test P256Verify")
-	// privateKey, err := ecdsa.GenerateKey(elliptic.P256(), cryptorand.Reader)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// publicKey := privateKey.PublicKey
-	// messageHash := crypto.Keccak256Hash(message)
-	// signature, err := crypto.Sign(messageHash.Bytes(), privateKey)
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// // Prepare input data for ecRecover precompiled contract
-	// inputData := make([]byte, 0, 160)
-	// inputData = append(inputData, messageHash.Bytes()...)
-	// inputData = append(inputData, signature...)
-	// inputData = append(inputData, publicKey.X.Bytes()...)
-	// inputData = append(inputData, publicKey.Y.Bytes()...)
 	ethBenchmarkInput := "4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e"
 	inputData, err := hex.DecodeString(ethBenchmarkInput)
 	if err != nil {
