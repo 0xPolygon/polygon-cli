fc323725d20ac87a7ebfaee958a73f28477645b2
jason.huang <20609724+doutv@users.noreply.github.com>
2024-11-13 15:36:16 +0800

    add source

--- bindings/tester/precompiles.go
+++ bindings/tester/precompiles.go
@@ -163,6 +163,8 @@ func GenerateBlake2FInput() []byte {
 }
 
 func GenerateP256VerifyInput() []byte {
+	// Ethereum benchmark input for P256Verify
+	// https://github.com/ethereum/go-ethereum/pull/30043/files#diff-b8e213cc8b44bc7d5d5e727524d63e19dd0f21312713ce2471948d1f64db212cR404
 	ethBenchmarkInput := "4cee90eb86eaa050036147a12d49004b6b9c72bd725d39d4785011fe190f0b4da73bd4903f0ce3b639bbbf6e8e80d16931ff4bcf5993d58468e8fb19086e8cac36dbcd03009df8c59286b162af3bd7fcc0450c9aa81be5d10d312af6c66b1d604aebd3099c618202fcfe16ae7770b0c49ab5eadf74b754204a3bb6060e44eff37618b065f9832de4ca6ca971a7a1adc826d0f7c00181a5fb2ddf79ae00b4e10e"
 	inputData, err := hex.DecodeString(ethBenchmarkInput)
 	if err != nil {
