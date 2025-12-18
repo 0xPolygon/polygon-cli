13cf7666afac27d2c0a440fc002dbb16ee5dc383
leovct <l009.vincent@gmail.com>
2025-12-18 08:12:10 +0000

    fix: var initialization

--- util/util.go
+++ util/util.go
@@ -431,7 +431,8 @@ func GetHexString(data any) string {
 	if reflect.TypeOf(data).Kind() == reflect.Float64 {
 		result = fmt.Sprintf("%x", int64(data.(float64)))
 	} else if reflect.TypeOf(data).Kind() == reflect.String {
-		result, ok := strings.CutPrefix(data.(string), "0x")
+		var ok bool
+		result, ok = strings.CutPrefix(data.(string), "0x")
 		if !ok {
 			result = data.(string)
 		}
