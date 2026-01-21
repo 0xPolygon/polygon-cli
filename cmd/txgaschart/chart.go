92afaea8267c12988509926199f75ee0021f6b74
Minh Vu <mvu@polygon.technology>
2026-01-21 10:19:56 -0500

    chore: combine like files

--- cmd/txgaschart/chart.go
+++ cmd/txgaschart/chart.go
@@ -375,3 +375,31 @@ func scaleGasToGasPrice(gasLimit uint64, metadata txGasChartMetadata) float64 {
 	}
 	return y
 }
+
+// ThickCrossGlyph draws an 'X' with configurable stroke width.
+type ThickCrossGlyph struct {
+	Width vg.Length
+}
+
+// DrawGlyph implements the GlyphDrawer interface.
+func (g ThickCrossGlyph) DrawGlyph(c *draw.Canvas, sty draw.GlyphStyle, p vg.Point) {
+	if !c.Contains(p) {
+		return
+	}
+	r := sty.Radius
+	ls := draw.LineStyle{Color: sty.Color, Width: g.Width}
+
+	// Horizontal
+	h := []vg.Point{{X: p.X - r, Y: p.Y}, {X: p.X + r, Y: p.Y}}
+	// Vertical
+	v := []vg.Point{{X: p.X, Y: p.Y - r}, {X: p.X, Y: p.Y + r}}
+	// Diagonal 1 (top-left -> bottom-right)
+	d1 := []vg.Point{{X: p.X - r, Y: p.Y + r}, {X: p.X + r, Y: p.Y - r}}
+	// Diagonal 2 (bottom-left -> top-right)
+	d2 := []vg.Point{{X: p.X - r, Y: p.Y - r}, {X: p.X + r, Y: p.Y + r}}
+
+	c.StrokeLines(ls, h)
+	c.StrokeLines(ls, v)
+	c.StrokeLines(ls, d1)
+	c.StrokeLines(ls, d2)
+}
--- cmd/txgaschart/cmd.go
+++ cmd/txgaschart/cmd.go
@@ -2,6 +2,7 @@ package txgaschart
 
 import (
 	"context"
+	_ "embed"
 	"encoding/json"
 	"fmt"
 	"math"
@@ -19,6 +20,49 @@ import (
 	"github.com/spf13/cobra"
 	"golang.org/x/time/rate"
 )
+
+type args struct {
+	rpcURL      string
+	rateLimit   float64
+	concurrency uint64
+
+	scale string
+
+	startBlock uint64
+	endBlock   uint64
+
+	targetAddr string
+
+	output string
+}
+
+var inputArgs = args{}
+
+//go:embed usage.md
+var usage string
+var Cmd = &cobra.Command{
+	Use:   "tx-gas-chart",
+	Short: "plot a chart of transaction gas prices and limits",
+	Long:  usage,
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, _ []string) error {
+		return buildChart(cmd)
+	},
+}
+
+func init() {
+	f := Cmd.PersistentFlags()
+	f.StringVar(&inputArgs.rpcURL, "rpc-url", "http://localhost:8545", "RPC URL of network")
+	f.Float64Var(&inputArgs.rateLimit, "rate-limit", 4, "requests per second limit (use negative value to remove limit)")
+	f.Uint64VarP(&inputArgs.concurrency, "concurrency", "c", 1, "number of tasks to perform concurrently (default: one at a time)")
+
+	f.StringVar(&inputArgs.scale, "scale", "log", "scale for gas price axis (options: log, linear)")
+
+	f.Uint64Var(&inputArgs.startBlock, "start-block", math.MaxUint64, "starting block number (inclusive)")
+	f.Uint64Var(&inputArgs.endBlock, "end-block", math.MaxUint64, "ending block number (inclusive)")
+	f.StringVar(&inputArgs.targetAddr, "target-address", "", "address that will have tx sent from or to highlighted in the chart")
+	f.StringVarP(&inputArgs.output, "output", "o", "tx_gasprice_chart.png", "where to save the chart image (default: tx_gasprice_chart.png)")
+}
 
 // txGasChartConfig holds the configuration for generating the transaction gas chart.
 type txGasChartConfig struct {
